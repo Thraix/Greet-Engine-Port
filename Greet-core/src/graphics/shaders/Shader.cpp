996ff73bc1307fa17ebe37e7bb86fdefe6db4124
Thraix <thraix@hotmail.com>
2019-05-29 12:41:50 +0200

    Store uniform locations in map

--- Greet-core/src/graphics/shaders/Shader.cpp
+++ Greet-core/src/graphics/shaders/Shader.cpp
@@ -16,30 +16,32 @@ namespace Greet {
   }
 
   Shader::Shader(const std::string& filename)
-    : m_shaderID{new uint{Load(filename)}}
+    : m_shaderID{new uint{Load(filename)}}, uniforms{GenUniforms(*m_shaderID.get())}
   {
+    Log::Info(filename);
     hotswap = std::make_optional(HotSwapping::AddHotswapResource(this, filename));
-    Log::Error(hotswap.has_value(), "Optional has no value after set");
+    if(!hotswap.has_value())
+      Log::Error("Optional has no value after set");
     Log::Info(*m_shaderID.get());
   }
 
   Shader::Shader(const std::string& vertSrc, const std::string& fragSrc, const std::string& geomSrc)
-    : m_shaderID{new uint{Load(vertSrc, fragSrc, geomSrc, true)}}
+    : m_shaderID{new uint{Load(vertSrc, fragSrc, geomSrc, true)}}, uniforms{GenUniforms(*m_shaderID.get())}
   {
 
   }
 
   Shader::Shader(const std::string& vertSrc, const std::string& fragSrc)
-    : m_shaderID{new uint{Load(vertSrc,fragSrc)}}
+    : m_shaderID{new uint{Load(vertSrc,fragSrc)}}, uniforms{GenUniforms(*m_shaderID.get())}
   {
 
   }
 
   Shader::Shader(Shader&& shader)
-    :
-      m_shaderID{std::move(shader.m_shaderID)}, hotswap{std::move(shader.hotswap)}
+    : m_shaderID{std::move(shader.m_shaderID)}, hotswap{std::move(shader.hotswap)}, uniforms{std::move(shader.uniforms)}
   {
     Log::Info("Moved address: ", this);
+    Log::Info("Uniforms size after move: ", uniforms.size());
     if(hotswap.has_value())
     {
       (*hotswap)->second.MoveResource(this);
@@ -50,6 +52,7 @@ namespace Greet {
   Shader& Shader::operator=(Shader&& shader)
   {
     m_shaderID = std::move(shader.m_shaderID);
+    uniforms = std::move(shader.uniforms);
     hotswap = std::move(shader.hotswap);
 
     if(hotswap.has_value())
@@ -128,6 +131,35 @@ namespace Greet {
     return program;
   }
 
+  std::map<std::string, int> Shader::GenUniforms(const uint program)
+  {
+    std::map<std::string, int> uniforms;
+    GLint numActiveUniforms = 0;
+    glGetProgramiv(program, GL_ACTIVE_UNIFORMS, &numActiveUniforms);
+    std::vector<GLchar> nameData(256);
+    for(int i= 0; i< numActiveUniforms; ++i)
+    {
+      GLint arraySize = 0;
+      GLenum type = 0;
+      GLsizei actualLength = 0;
+      glGetActiveUniform(program, i, nameData.size(), &actualLength, &arraySize, &type, &nameData[0]);
+
+      // For some reason arrays return 'arrayName[0]'
+      for(int i = 0;i<actualLength;i++)
+      {
+        if(nameData[i] == '[')
+        {
+          nameData[i] = '\0';
+          break;
+        }
+      }
+      std::string name(nameData.data());
+      uniforms.emplace(name, glGetUniformLocation(program, name.c_str()));
+      Log::Info("Found uniform \'", name, "\' ", arraySize);
+    }
+    return uniforms;
+  }
+
   uint Shader::AttachShader(const uint program, const std::string& shaderSrc, const uint shaderType)
   {
     GLCall(uint shader = glCreateShader(shaderType));
@@ -180,68 +212,70 @@ namespace Greet {
     GLCall(glBindFragDataLocation(*m_shaderID,attachmentId,name.c_str()));
   }
 
-  uint Shader::GetUniformLocation(const char *name) const
+  int Shader::GetUniformLocation(const std::string& name) const
   {
-    GLCall(uint location = glGetUniformLocation(*m_shaderID,name));
-    return location;
+    auto it = uniforms.find(name);
+    if(it != uniforms.end())
+      return it->second;
+    return -1;
   }
 
-  void Shader::SetUniformBoolean(const char *name, bool value) const
+  void Shader::SetUniformBoolean(const std::string& name, bool value) const
   {
     GLCall(glUniform1f(GetUniformLocation(name), value ? 1.0f : 0.0f));
   }
 
-  void Shader::SetUniform1f(const char *name, float value) const
+  void Shader::SetUniform1f(const std::string& name, float value) const
   {
     GLCall(glUniform1f(GetUniformLocation(name), value));
   }
 
-  void Shader::SetUniform1fv(const char *name, uint count, float* value) const
+  void Shader::SetUniform1fv(const std::string& name, uint count, float* value) const
   {
     GLCall(glUniform1fv(GetUniformLocation(name), count, value));
   }
 
-  void Shader::SetUniform1i(const char *name, int value) const
+  void Shader::SetUniform1i(const std::string& name, int value) const
   {
     GLCall(glUniform1i(GetUniformLocation(name), value));
   }
 
-  void Shader::SetUniform1iv(const char *name, uint count, int* value) const
+  void Shader::SetUniform1iv(const std::string& name, uint count, int* value) const
   {
     GLCall(glUniform1iv(GetUniformLocation(name), count, value));
   }
 
-  void Shader::SetUniform1ui(const char *name, uint value) const
+  void Shader::SetUniform1ui(const std::string& name, uint value) const
   {
     GLCall(glUniform1ui(GetUniformLocation(name), value));
   }
 
-  void Shader::SetUniform1uiv(const char *name, uint count, uint* value) const
+  void Shader::SetUniform1uiv(const std::string& name, uint count, uint* value) const
   {
     GLCall(glUniform1uiv(GetUniformLocation(name), count, value));
   }
 
-  void Shader::SetUniform2f(const char *name, const Vec2 &value) const
+  void Shader::SetUniform2f(const std::string& name, const Vec2 &value) const
   {
     GLCall(glUniform2f(GetUniformLocation(name), value.x, value.y));
   }
 
-  void Shader::SetUniform3f(const char *name, const Vec3<float>& value) const
+  void Shader::SetUniform3f(const std::string& name, const Vec3<float>& value) const
   {
     GLCall(glUniform3f(GetUniformLocation(name), value.x, value.y, value.z));
   }
 
-  void Shader::SetUniform4f(const char *name, const Vec4 &value) const
+  void Shader::SetUniform4f(const std::string& name, const Vec4 &value) const
   {
     GLCall(glUniform4f(GetUniformLocation(name), value.x, value.y, value.z, value.w));
   }
 
-  void Shader::SetUniformMat3(const char *name, const Mat3 &value) const
+  void Shader::SetUniformMat3(const std::string& name, const Mat3 &value) const
   {
     GLCall(glUniformMatrix3fv(GetUniformLocation(name), 1, GL_FALSE, value.elements));
   }
 
-  void Shader::SetUniformMat4(const char *name, const Mat4 &value) const
+  void Shader::SetUniformMat4(const std::string& name, const Mat4 &value) const
   {
     GLCall(glUniformMatrix4fv(GetUniformLocation(name), 1, GL_FALSE, value.elements));
   }
--- Greet-core/src/graphics/shaders/Shader.h
+++ Greet-core/src/graphics/shaders/Shader.h
@@ -20,6 +20,7 @@ namespace Greet {
   {
     private:
       std::unique_ptr<uint, ShaderDeleter> m_shaderID;
+      std::map<std::string, int> uniforms;
       std::optional<std::map<uint, HotswapResource>::iterator> hotswap;
 
     private:
@@ -31,7 +32,7 @@ namespace Greet {
       uint Load(const std::string& vertSrc, const std::string& fragSrc, const std::string& geomSrc, bool hasGeometry);
       uint Load(const std::string& vertSrc, const std::string& fragSrc);
       uint AttachShader(const uint program, const std::string& shaderSrc, const uint shaderType);
-      uint GetUniformLocation(const char*name) const;
+      int GetUniformLocation(const std::string& name) const;
 
     public:
       Shader(Shader&&);
@@ -42,19 +43,20 @@ namespace Greet {
       void Enable() const;
       static void Disable();
       void BindAttributeOutput(uint attachmentId, const std::string& name) const;
+      std::map<std::string, int> GenUniforms(const uint program);
 
-      void SetUniformBoolean(const char *name, bool value) const;
-      void SetUniform1f(const char *name, float value) const;
-      void SetUniform1fv(const char *name, uint count, float* value) const;
-      void SetUniform1i(const char *name, int value) const;
-      void SetUniform1iv(const char *name, uint count, int* value) const;
-      void SetUniform1ui(const char *name, uint value) const;
-      void SetUniform1uiv(const char *name, uint count, uint* values) const;
-      void SetUniform2f(const char *name, const Vec2& value) const;
-      void SetUniform3f(const char *name, const Vec3<float>& value) const;
-      void SetUniform4f(const char *name, const Vec4& value) const;
-      void SetUniformMat3(const char *name, const Mat3 &value) const;
-      void SetUniformMat4(const char *name, const Mat4 &value) const;
+      void SetUniformBoolean(const std::string& name, bool value) const;
+      void SetUniform1f(const std::string& name, float value) const;
+      void SetUniform1fv(const std::string& name, uint count, float* value) const;
+      void SetUniform1i(const std::string& name, int value) const;
+      void SetUniform1iv(const std::string& name, uint count, int* value) const;
+      void SetUniform1ui(const std::string& name, uint value) const;
+      void SetUniform1uiv(const std::string& name, uint count, uint* values) const;
+      void SetUniform2f(const std::string& name, const Vec2& value) const;
+      void SetUniform3f(const std::string& name, const Vec3<float>& value) const;
+      void SetUniform4f(const std::string& name, const Vec4& value) const;
+      void SetUniformMat3(const std::string& name, const Mat3 &value) const;
+      void SetUniformMat4(const std::string& name, const Mat4 &value) const;
       bool operator<(const Shader& s);
     public:
       static Shader FromFile(const std::string& shaderPath);
